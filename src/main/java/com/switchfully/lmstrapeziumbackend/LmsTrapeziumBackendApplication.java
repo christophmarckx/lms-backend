092d2af5ea17703d896188bcf91b83af603f679c
Louis Fitdevoie <fitdevoielouis@hotmail.be>
2024-05-24 15:38:50 +0200

    Initial commit

---
+++ src/main/java/com/switchfully/lmstrapeziumbackend/LmsTrapeziumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.lmstrapeziumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsTrapeziumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsTrapeziumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/switchfully/lmstrapeziumbackend/LmsTrapeziumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.lmstrapeziumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsTrapeziumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
