8e0ec89cc443b29b79006ac25b35966c96b63e92
Michael Koene <michael.koene@skynet.be>
2024-05-29 12:54:59 +0200

    Change ACC1 implementation : create user in keycloak via backend

--- src/main/java/com/switchfully/lmstrapeziumbackend/user/StudentService.java
+++ src/main/java/com/switchfully/lmstrapeziumbackend/user/StudentService.java
@@ -1,21 +1,27 @@
 package com.switchfully.lmstrapeziumbackend.user;
 
+import com.switchfully.lmstrapeziumbackend.security.KeycloakService;
 import com.switchfully.lmstrapeziumbackend.user.dto.CreateStudentDTO;
 import com.switchfully.lmstrapeziumbackend.user.dto.StudentDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class StudentService {
 
     private UserRepository userRepository;
     private StudentMapper studentMapper;
+    private KeycloakService keycloakService;
 
-    public StudentService(UserRepository userRepository, StudentMapper studentMapper) {
+    public StudentService(UserRepository userRepository, StudentMapper studentMapper, KeycloakService keycloakService) {
         this.userRepository = userRepository;
         this.studentMapper = studentMapper;
+        this.keycloakService = keycloakService;
     }
 
     public StudentDTO createStudent(CreateStudentDTO createStudentDTO) {
-        return this.studentMapper.toDTO(this.userRepository.save(this.studentMapper.toEntity(createStudentDTO)));
+        UUID userKeycloakId = this.keycloakService.createUser(createStudentDTO);
+        return this.studentMapper.toDTO(this.userRepository.save(this.studentMapper.toEntity(userKeycloakId, createStudentDTO)));
     }
 }
--- src/main/java/com/switchfully/lmstrapeziumbackend/user/dto/CreateStudentDTO.java
+++ src/main/java/com/switchfully/lmstrapeziumbackend/user/dto/CreateStudentDTO.java
@@ -7,10 +7,13 @@ public record CreateStudentDTO(
         @NotEmpty(message = EMAIL_REQUIRED_MESSAGE)
         @Email(message = EMAIL_FORMAT_MESSAGE)
         String email,
+        @NotEmpty(message = PASSWORD_REQUIRED_MESSAGE)
+        String password,
         @NotEmpty(message = DISPLAY_NAME_REQUIRED_MESSAGE)
         String displayName
 ) {
     public static final String EMAIL_REQUIRED_MESSAGE = "Email is required";
     public static final String EMAIL_FORMAT_MESSAGE = "Email format must be x@x.x";
     public static final String DISPLAY_NAME_REQUIRED_MESSAGE = "Display name is required";
+    public static final String PASSWORD_REQUIRED_MESSAGE = "Password is required";
 }
