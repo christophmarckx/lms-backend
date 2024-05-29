53a26bb7f8bee5e17b948103593bde3827880e24
BODDAERT SIMON <simon.boddaert@minfin.fed.be>
2024-05-29 15:23:14 +0200

    added PUT parts to E2E test

--- src/test/java/com/switchfully/lmstrapeziumbackend/TestConstants.java
+++ src/test/java/com/switchfully/lmstrapeziumbackend/TestConstants.java
@@ -5,9 +5,8 @@ import com.switchfully.lmstrapeziumbackend.classgroup.dto.ClassgroupDTO;
 import com.switchfully.lmstrapeziumbackend.classgroup.dto.CreateClassgroupDTO;
 import com.switchfully.lmstrapeziumbackend.course.Course;
 import com.switchfully.lmstrapeziumbackend.course.dto.CourseDTO;
-import com.switchfully.lmstrapeziumbackend.module.Module;
+import com.switchfully.lmstrapeziumbackend.course.dto.UpdateCourseDTO;
 import com.switchfully.lmstrapeziumbackend.course.dto.CreateCourseDTO;
-import com.switchfully.lmstrapeziumbackend.module.dto.CreateModuleDTO;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,11 +19,20 @@ public class TestConstants {
     public static final CreateCourseDTO CREATE_COURSE_DTO_1 = new CreateCourseDTO("Java@Fin", "The Java@Fin course");
     public static final Course COURSE_1 = new Course(CREATE_COURSE_DTO_1.getName(), CREATE_COURSE_DTO_1.getDescription());
     public static final CourseDTO COURSE_DTO_1 = new CourseDTO(UUID.fromString("f953c154-36f2-4b79-8992-b6f5d4dd24a9"), COURSE_1.getName(), COURSE_1.getDescription());
+    public static final UpdateCourseDTO UPDATED_COURSE_1 = new UpdateCourseDTO("NEW UPDATED NAME");
     public static Map<String, Object> getExpectedMapForFullyInvalidCreateCourseDTO() {
         Map<String, Object> mapExpected = new HashMap<>();
         Map<String, String> errorsMap = new HashMap<>();
         errorsMap.put("name", "Name must be between 2 and 255 characters");
         mapExpected.put("message", "Following validation error(s) occurred on /courses");
+        mapExpected.put("errors", errorsMap);
+        return mapExpected;
+    }
+    public static Map<String, Object> getExpectedMapForFullyInvalidUpdateCourseDTO() {
+        Map<String, Object> mapExpected = new HashMap<>();
+        Map<String, String> errorsMap = new HashMap<>();
+        errorsMap.put("name", "Name must be between 2 and 255 characters");
+        mapExpected.put("message", "Following validation error(s) occurred on /courses/" + COURSE_DTO_1.getId());
         mapExpected.put("errors", errorsMap);
         return mapExpected;
     }
