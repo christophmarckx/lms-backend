765e66d70670ff5c3b9491b0d218dec9b322df6c
Michael Koene <michael.koene@skynet.be>
2024-05-30 19:07:31 +0200

    Set security for /students

--- src/main/java/com/switchfully/lmstrapeziumbackend/security/WebSecurityConfig.java
+++ src/main/java/com/switchfully/lmstrapeziumbackend/security/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.switchfully.lmstrapeziumbackend.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
